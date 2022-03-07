a6a944766d077b7622d6e45d6ae72f08a1901bfc
MalkovGN <malkovgn@gmail.com>
2022-03-07 20:24:32 +0300

    Adding a project

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField(blank=True)
+    date = models.DateField(blank=True)
+    description = models.CharField(max_length=250)
+
