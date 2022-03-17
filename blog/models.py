497c24a78778586886f377cd6dbae50a09280a90
MalkovGN <malkovgn@gmail.com>
2022-03-17 22:10:17 +0300

    Addind a Bootstrap templates

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    # url = models.URLField(blank=True)
+    url = models.URLField(blank=True)
     date = models.DateField(blank=True)
-    description = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')
+    blogs = Blog.objects.all()
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
